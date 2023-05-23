d60c97c4cf5749a930af3e02d9604b5f845150ef
afunmibi <112468468+afunmibi@users.noreply.github.com>
2023-05-23 04:36:32 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// form validation 
+
+let fname = document.getElementById('fname');
+let sname = document.getElementById('sname');
+let phone = document.getElementById('phone');
+let email = document.getElementById('email');
+let dob = document.getElementById('dob');
+let children = document.getElementById('children');
+let radio = document.getElementById('radio');
+
+    
+    
+   /* function validate(){
+        if(fname.value ===""){
+            alert('First Name must be filled');
+            return false;
+        }else if(sname.value ===""){
+            alert('Surname must be provided');
+                return false;
+            }else if(phone.value ===""){
+                alert('Phone number must be provided');
+                return false;
+
+            }else if(email.value ===""){
+                alert('email must be provided');
+                return false;
+
+            }else if(dob.value === ""){
+                alert('Date of Birth must be provided');
+                return false; 
+
+            }else if(radio.value ===""){
+                alert('You must select the Gender!');
+                return false;
+
+            }else if(children.value ===""){
+                alert('No. of children must be provided');
+                return false;
+
+            
+
+            }else{
+                alert('Your message has been recorded')
+            }
+
+            }*/
+        
+            // form validation using logical operator
+            function validate(){
+
+            if(fname.value==='' || sname.value ==='' || phone.value===''  || email.value===''  || dob.value===''  || children.value==='' || radio.value===''){
+                alert('Input must be filled');
+                return false;
+            }else{
+                alert('Message received.')
+                return true;
+            }
+        }
